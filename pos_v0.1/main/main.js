4de68ce22ee8eb038ef429c2b3b30fe6d2fb9dee
Ubuntu <835943102@qq.com>
2017-04-08 21:45:53 +0800

    update

--- pos_v0.1/main/main.js
+++ pos_v0.1/main/main.js
@@ -1,5 +1,33 @@
 'use strict';
 
 function printReceipt(inputs) {
-  console.log('请在此实现练习要求，并改写该行代码。');
+  let arr = new Array();
+  let k = 0, i;
+  let sum = 0, count = 1, string = "***<没钱赚商店>收据***\n";
+
+  for (i = 0; i < inputs.length - 1; i++) {
+
+    if (inputs[i].name == inputs[i + 1].name) {
+      count++;
+    }
+
+    else {
+      sum = sum + count * inputs[i].price;
+      arr.push("名称：" + inputs[i].name + "，数量：" + count + inputs[i].unit + "，" + "单价：" + inputs[i].price + ".00(元)，" + "小计：" + count * inputs[i].price + ".00(元)\n");
+      count = 1;
+
+      string = string + arr[k++];
+    }
+
+  }
+  if (i + 1 == inputs.length) {
+
+    arr.push("名称：" + inputs[i].name + "，数量：" + count + inputs[i].unit + "，" + "单价：" + inputs[i].price + ".00(元)，" + "小计：" + count * inputs[i].price + ".00(元)\n");
+
+  }
+  string = string + arr[k++];
+  sum += inputs[i].price;
+  string = string + "----------------------\n" + "总计：" + sum + ".00(元)\n" + "**********************";
+
+  console.log(string);
 }
