33e14295c0308d5a7d3f8753a8902dcfcde9f37b
Ubuntu <835943102@qq.com>
2017-04-08 22:17:10 +0800

    update

--- pos_v0.1/main/main.js
+++ pos_v0.1/main/main.js
@@ -5,14 +5,15 @@ function printReceipt(inputs) {
   let k = 0, i;
   let sum = 0, count = 1, string = "***<没钱赚商店>收据***\n";
 
-  for (i = 0; i < inputs.length - 1; i++) {
+  for (i = 0; i < inputs.length - 1; i++) {     //时间复杂度O(N),下标计算到i+1,所以循环i累加到数组长度减一
 
-    if (inputs[i].name == inputs[i + 1].name) {
+    if (inputs[i].name == inputs[i + 1].name) {      //比较输入的前一个对象名是否和后一个对象名相等
       count++;
     }
 
-    else {
+    else {                    //若对象名不相等则计算该商品总价,将其信息存入数组arr,count重置为1
       sum = sum + count * inputs[i].price;
+
       arr.push("名称：" + inputs[i].name + "，数量：" + count + inputs[i].unit + "，" + "单价：" + inputs[i].price + ".00(元)，" + "小计：" + count * inputs[i].price + ".00(元)\n");
       count = 1;
 
@@ -20,7 +21,8 @@ function printReceipt(inputs) {
     }
 
   }
-  if (i + 1 == inputs.length) {
+
+  if (inputs[i-1].name != inputs[i].name) {     //若最后一个对象名不等于倒数第二个对象名则将最后一个商品个数为一
 
     arr.push("名称：" + inputs[i].name + "，数量：" + count + inputs[i].unit + "，" + "单价：" + inputs[i].price + ".00(元)，" + "小计：" + count * inputs[i].price + ".00(元)\n");
 
